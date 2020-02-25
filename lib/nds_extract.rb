dab47586fea54f8fe6298b25b39f1588c1364cdd
Will Brennan <willfbren@gmail.com>
2020-02-25 20:07:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,6 +21,6 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   while directors_index < nds.length do
     directors_name = nds[directors_index][:name]
-    
+    result = [directors_name]
   nil
 end
