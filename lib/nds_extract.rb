d5864da9c297a78d56ae1859b716181d85609396
Will Brennan <willfbren@gmail.com>
2020-02-25 20:09:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,6 +25,6 @@ def directors_totals(nds)
     movie_index = 0 
     
     while movie_index < nds[directors_index][:movies].length do
-      result[directors_name] += 
+      result[directors_name] += nds[directors_index][:movie]
   nil
 end
